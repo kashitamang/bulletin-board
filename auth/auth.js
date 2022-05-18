66bc0a4661b86dc92cba0915ff820241718cacb1
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-18 12:28:09 -0700

    updated to do list

--- auth/auth.js
+++ auth/auth.js
@@ -12,7 +12,7 @@ redirectIfLoggedIn();
 signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(signUpForm);
-    console.log({ email: data.get('email'), password: data.get('password') });
+    //console.log({ email: data.get('email'), password: data.get('password') });
     const user = await signupUser(data.get('email'), data.get('password'));
     //redirect to home page on successful authorization
     if (user) {
@@ -25,7 +25,7 @@ signUpForm.addEventListener('submit', async (e) => {
 signInForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(signInForm);
-    console.log({ email: data.get('email'), password: data.get('password') });
+    //console.log({ email: data.get('email'), password: data.get('password') });
     const user = await signInUser(data.get('email'), data.get('password'));
     //redirect to other page on successful auth
     if (user) {
--- create/create.js
+++ create/create.js
@@ -8,7 +8,7 @@ checkAuth();
 createForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(createForm);
-    console.log({ title: data.get('title'), feeling: data.get('feeling'), confession: data.get('confession') });
+    //console.log({ title: data.get('title'), feeling: data.get('feeling'), confession: data.get('confession') });
     const newPost = {
         title: data.get('title'),
         feeling: data.get('feeling'),
